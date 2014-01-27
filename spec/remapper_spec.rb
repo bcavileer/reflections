b427f1b76c26bf5eee4e5704bd92cc0a59e1d06b
Benjamin Cavileer <bcavileer@holmanauto.com>
2014-01-27 15:10:58 -0500

    Cleanup remapper_spec

--- spec/remapper_spec.rb
+++ spec/remapper_spec.rb
@@ -1,16 +1,12 @@
 require 'support/spec_helper'
 
 describe Reflections::Remapper do
-
+  before(:each) { DatabaseCleaner.clean }
   let(:user1) { User.create }
   let(:user2) { User.create }
 
-  before(:each) { DatabaseCleaner.clean }
-
   describe 'Remapping to a different class' do
     it 'raises an error' do
-      class OtherClass < ActiveRecord::Base; end
-
       expect { user1.map_belongs_to_associations_to OtherClass.new }.
           to raise_error(Reflections::Remapper::NotSameClass)
     end
@@ -18,28 +14,18 @@ describe Reflections::Remapper do
 
   describe 'default belongs_to' do
     it 're-maps default belongs_to associations' do
-      class Widget < ActiveRecord::Base
-        belongs_to :user
-      end
-
+      Widget.belongs_to :user
       Widget.create :user => user1
-
       user1.map_belongs_to_associations_to user2
-
       Widget.first.user.should eq(user2)
     end
   end
 
   describe 'Remapping custom named belongs_to' do
     it 're-maps custom named belongs_to associations' do
-      class Widget < ActiveRecord::Base
-        belongs_to :creator, :class_name => 'User'
-      end
-
+      Widget.belongs_to :creator, :class_name => 'User'
       Widget.create :creator => user1
-
       user1.map_belongs_to_associations_to user2
-
       Widget.first.creator.should eq(user2)
     end
   end
